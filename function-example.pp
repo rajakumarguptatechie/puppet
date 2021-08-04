39a0f6dbe7c6cf451b70f5e72744d7cb00275211
rajakumarguptatechie <56320038+rajakumarguptatechie@users.noreply.github.com>
2021-08-04 08:13:25 +0530

    Update function-example.pp

--- function-example.pp
+++ function-example.pp
@@ -1,3 +1,8 @@
+# Function syntax (written in ruby)
+Puppet::Functions.create_function(:<function-name>) do
+  write-your-code
+done
+
 # Function example
 $my_arr = [1, 2, 3,  4, 5]
 $my_arr.each |$my_var| {
