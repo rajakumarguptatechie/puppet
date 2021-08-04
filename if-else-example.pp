4a9cbc828d44c709771feda3d4ed1d96739c1a61
rajakumarguptatechie <56320038+rajakumarguptatechie@users.noreply.github.com>
2021-08-04 08:12:33 +0530

    Update if-else-example.pp

--- if-else-example.pp
+++ if-else-example.pp
@@ -1,5 +1,44 @@
 ## Puppet DSl examples
+# if-else syntax
+1. Simple if
+if <condition> {
+  do-something01
+  do-something02
+}
+
+2. If-else
+if <condition> {
+  do-something01
+  do-something02
+} else {
+  do-something01
+  do-something02
+}
+
+3. If-elsif
+if <condition> {
+  do-something01
+  do-something02
+} elsif <condition> {
+  do-something01
+  do-something02
+} else {
+  do-something01
+  do-something02
+}
+
+4. nested condition
+if <condition> {
+  if <condition> {
+    do-something01
+    do-something02
+  }
+  do-something01
+  do-something02
+}
+
 # if-else example
+# Example 01:
 if $facts['is_virtual'] {
   notify{'This is a virtual machine':}
 } elsif $facts['os']['family'] == 'Darwin' {
@@ -8,6 +47,7 @@ if $facts['is_virtual'] {
   notify{'No condition is met':}
 }
 
+# Example 02:
 $a=5
 $b=3
 if $a == $b {
