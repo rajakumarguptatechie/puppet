68e80ac28215baf8d55ba2e43d3bf435b9221f19
rajakumarguptatechie <56320038+rajakumarguptatechie@users.noreply.github.com>
2021-08-04 08:19:14 +0530

    Update relationship-example.pp

--- relationship-example.pp
+++ relationship-example.pp
@@ -1,9 +1,36 @@
-# Relationship parameter example
-file {'/var/tmp/y':
-  ensure  => file,
-  require => File['/var/tmp/x'],
+Requirement:
+1. config file must be created and then only
+2. service should be started
+
+Scenarios:
+a. strict usual order
+b. override usual order
+
+Relationship meta parameters:
+
+    before: Applies a resource before the target resource.
+
+    require: Applies a resource after the target resource.
+
+    notify: Applies a resource before the target resource. The target resource refreshes if the notifying resource changes.
+
+    subscribe: Applies a resource after the target resource. The subscribing resource refreshes if the target resource changes.
+
+# Examples:
+file{'/etc/init.d/abc.conf':
+  ensure => present,
+  before => Service['abc'] 
 }
-file {'/var/tmp/x':
-  ensure => file,
+service{'abc':
+  ensure => reload,
+  enable => true,
 }
 
+file{'/etc/init.d/abc.conf':
+  ensure => present,
+}
+service{'abc':
+  ensure  => running,
+  enable  => true,
+  require => File['/etc/init.d/abc.conf'],
+}
